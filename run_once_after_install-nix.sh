ebb08dad28d10b101c5372ee84ebf24531a6fd9f
Xavier Maillard <xa.maillard@gmail.com>
2022-11-26 15:18:33 +0100

    update nix install script

--- run_once_after_install-nix.sh
+++ run_once_after_install-nix.sh
@@ -8,6 +8,7 @@ fi
 
 (
 if [ "$(command -v nix)" ] && ! [ "$(command -v home-manager)" ]; then
+  export NIX_PATH="$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}"
   nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
   nix-channel --update
   nix-shell '<home-manager>' -A install
