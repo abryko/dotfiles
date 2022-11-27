fc0c58bc45dc8b412c4931dad8f06e0eafb36519
Xavier Maillard <xavier.maillard@orange.com>
2022-11-27 14:24:06 +0100

    Restore gnome & remove terminator

--- dot_config/nixpkgs/home.nix
+++ dot_config/nixpkgs/home.nix
@@ -1,8 +1,5 @@
 { config, pkgs, ... }:
 
-let 
-  myFontPkg = pkgs.nerdfonts.override { fonts = [ "BitstreamVeraSansMono" "DejaVuSansMono" "Noto" "Ubuntu" "UbuntuMono" ]; };
-in
 {
 
   imports = [
@@ -42,10 +39,12 @@ in
     jc
     jq
     libnotify
+    (nerdfonts.override {
+      fonts = [ "BitstreamVeraSansMono" "DejaVuSansMono" "Noto" "Ubuntu" "UbuntuMono" ];
+    })
     niv
     nixfmt
     nmap
-    myFontPkg
     noto-fonts-emoji
     open-policy-agent
     pre-commit
@@ -69,61 +68,50 @@ in
 
   targets.genericLinux.enable = true;
 
-  #dconf.settings = {
-  #  "com/gexperts/Tilix" = {
-  #    enable-wide-handle = false;
-  #    paste-strip-trailing-whitespace = true; 
-  #    prompt-on-close = true;
-  #    prompt-on-delete-profile = true;
-  #    quake-specific-monitor = 0;
-  #    terminal-title-show-when-single = true;
-  #    terminal-title-style = "small";
-  #    theme-variant = "dark";
-  #    use-tabs = true;
-  #  };
-  #  "com/gexperts/Tilix/keybindings" = {
-  #    session-add-down = "<Primary><Shift>o";
-  #    session-add-right = "<Primary><Shift>i";
-  #    session-open = "disabled";
-  #    session-synchronize-input = "<Primary><Shift>g";
-  #    terminal-find-next = "disabled";
-  #  };
-  #  "com/gexperts/Tilix/profiles/2b7c4080-0ddd-46c5-8f23-563fd3ba789d" = {
-  #    background-color = "#000000000000";
-  #    background-transparency-percent = 0;
-  #    badge-color = "#AC7EA8";
-  #    badge-color-set = true;
-  #    bold-color-set = false;
-  #    bold-is-bright = true;
-  #    cursor-colors-set = false;
-  #    dim-transparency-percent = 28;
-  #    font = "BitstreamVeraSansMono Nerd Font Mono 10";
-  #    foreground-color = "#EFEFEF";
-  #    highlight-colors-set = false;
-  #    palette = [
-  #      "#000000"
-  #      "#CC0000"
-  #      "#4D9A05"
-  #      "#C3A000"
-  #      "#3464A3"
-  #      "#754F7B"
-  #      "#05979A"
-  #      "#D3D6CF"
-  #      "#545652"
-  #      "#EF2828"
-  #      "#89E234"
-  #      "#FBE84F"
-  #      "#729ECF"
-  #      "#AC7EA8"
-  #      "#34E2E2"
-  #      "#EDEDEB"
-  #    ];
-  #    scroll-on-output = false;
-  #    scrollback-unlimited = true;
-  #    use-theme-colors = false;
-  #    visible-name = "Default";
-  #  };
-  #};
+  programs.gnome-terminal = {
+    enable = true;
+    themeVariant = "dark";
+    profile = {
+      "4dff060d-cd00-450a-888f-309315596f9b" = {
+        allowBold = true;
+        audibleBell = true;
+        backspaceBinding = "ascii-delete";
+        boldIsBright = true;
+        colors = {
+          backgroundColor = "#000000";
+          foregroundColor = "#ffffff";
+          palette = [
+            "#2e3436"
+            "#cc0000"
+            "#4e9a06"
+            "#c4a000"
+            "#3465a4"
+            "#75507b"
+            "#06989a"
+            "#d3d7cf"
+            "#555753"
+            "#ef2929"
+            "#8ae234"
+            "#fce94f"
+            "#729fcf"
+            "#ad7fa8"
+            "#34e2e2"
+            "#eeeeec"
+          ];
+        };
+        cursorBlinkMode = "on";
+        cursorShape = "block";
+        default = true;
+        deleteBinding = "delete-sequence";
+        scrollbackLines = 1000000;
+        scrollOnOutput = false;
+        showScrollbar = true;
+        transparencyPercent = null;
+        visibleName = "default";
+      };
+    };
+    showMenubar = false;
+  };
 
   editorconfig = {
     enable = true;
@@ -164,7 +152,6 @@ in
   gtk = {
     enable = true;
     font = {
-      package = myFontPkg;
       name = "BitstreamVeraSansMono Nerd Font Mono";
       size = 10;
     };
@@ -179,39 +166,6 @@ in
     theme = {
       package = pkgs.gnome.gnome-themes-extra;
       name = "Adwaita";
-    };
-  };
-
-  programs.terminator = {
-    enable = true;
-    config = {
-      global_config = {
-        scroll_tabbar = true;
-        title_transmit_bg_color = "#61996b";
-        inactive_color_offset = 0.80;
-        title_use_system_font = false;
-        title_font = "BitstreamVeraSansMono Nerd Font Mono 9";
-      };
-      keybindings = {
-        split_vert = "<Primary><Shift>i";
-        split_horiz = "<Primary><Shift>o";
-        toggle_zoom = "<Primary><Shift>x";
-        group_all = null;
-        group_all_toggle = "<Primary><Shift>h";
-        group_tab = null;
-        group_tab_toggle = "<Primary><Shift>g";
-        new_window = null;
-        reset_clear = null;
-      };
-      profiles.default = {
-        use_system_font = false;
-        font = "BitstreamVeraSansMono Nerd Font Mono Roman 10";
-        scroll_on_output = true;
-        scrollback_infinite = true;
-        background_color = "#000000";
-        foreground_color = "#ffffff";
-        palette = "#2e3436:#cc0000:#499105:#c4a000:#3968a3:#75507b:#06989a:#d3d7cf:#555753:#ef2929:#8ae234:#fce94f:#729fcf:#ad7fa8:#34e2e2:#eeeeec";
-      };
     };
   };
 
--- dot_config/nixpkgs/tilix/README.md
+++ dot_config/nixpkgs/tilix/README.md
@@ -1,7 +1,9 @@
 # How to fetch current config from Tilix
 ```
-dconf dump /com/gexperts/Tilix/ > tilix.conf
-dconf2nix -i tilix.conf -o default.nix -r com/gexperts/Tilix
+
+tmpfile="$(mktemp -u -t tilix-XXXXXX.conf)"
+dconf dump /com/gexperts/Tilix/ > "${tmpfile}"
+dconf2nix -i "${tmpfile}" -o default.nix -r com/gexperts/Tilix
 ```
 
 
