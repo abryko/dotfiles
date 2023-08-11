d6a7a1e49902bee18836dbaabace8c3254e5c5bb
Xavier Maillard <xavier.maillard@s3ns.io>
2023-08-11 19:21:03 +0200

    Update config

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -1434,14 +1434,14 @@ typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
 
   ####################################[ time: current time ]####################################
   # Current time color.
-  typeset -g POWERLEVEL9K_EXPERIMENTAL_TIME_REALTIME=false
+  typeset -g POWERLEVEL9K_EXPERIMENTAL_TIME_REALTIME=true
   typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
   # Format for the current time: 09:51:02. See `man 3 strftime`.
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   # If set to true, time will update when you hit enter. This way prompts for the past
   # commands will contain the start times of their commands as opposed to the default
   # behavior where they contain the end times of their preceding commands.
-  typeset -g POWERLEVEL9K_TIME_UPDATE_ON_COMMAND=true
+  typeset -g POWERLEVEL9K_TIME_UPDATE_ON_COMMAND=false
   # Custom icon.
   # typeset -g POWERLEVEL9K_TIME_VISUAL_IDENTIFIER_EXPANSION='⭐'
   # Custom prefix.
