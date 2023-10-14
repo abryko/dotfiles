2fe044eeb77a2fe022d0e172e5f696cbf70995d8
Xavier Maillard <xavier.maillard@s3ns.io>
2023-10-14 15:07:02 +0200

    nix: bump lock

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nickel = {
-      url = "github:tweag/nickel";
+      url = "github:tweag/nickel/1.2.2";
       # inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-index-database = {
@@ -27,11 +27,13 @@
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
       "https://numtide.cachix.org"
+      "https://tweag-nickel.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "tweag-nickel.cachix.org-1:GIthuiK4LRgnW64ALYEoioVUQBWs0jexyoYVeLDBwRA="
     ];
   };
 
