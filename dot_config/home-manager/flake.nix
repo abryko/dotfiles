3fe8bac62e6b0fcdd23c929cd4a61a2917aa91e7
Xavier Maillard <xavier.maillard@s3ns.io>
2024-04-20 18:17:25 +0200

    nix: bump config

--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
